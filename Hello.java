45047f7ab12c5bd622437e52a80c4fb6f3778352
Bhaveshkb <82366450+Bhaveshkb@users.noreply.github.com>
2021-05-08 23:33:56 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-class Hello
-{
-	public static void main(String [] args)
-	{
-		System.out.println("Hello World");
-		System.out.println("Welcome to CDAC Mumbai Kharghar");
-		System.out.println("Helll i am Bhavesh Kishor Bhangale I am here in cdac kharghar for next 5 months to persue E-Dac course");
-	}
-}
